9ad93211e3e3dbab5a369c452adea9d8b4dbc86a
haimyiuanh <haimy.nguyen0734@gmail.com>
2026-02-01 22:39:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,21 +9,17 @@ form.addEventListener("submit", function (e) {
     comment: document.getElementById("comment").value
   };
 
-  fetch("https://script.google.com/macros/s/AKfycbwMn_HZ-RvxdVp7D7w-nQfWxiWjkMekIlzzflcCf0LWaDu9ya6g7HXZ0Euz_AcAGu4F/exec", {
+  fetch("https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLixRnFzLzHGfnuLGoNcYFuH2Pi5bXtTzbES2njKUgaqflbFD8AXGUikLGVxkEGwg2D27I0o3txmSLQICI38bNqvIY17ybyuJIgW1H6zqjGTrflql9mBQFCbXh7H6TOEZztphunZ-pIZFFc-0t35tDE2MlS8Afu7xKbwFF7tlZr5Vbqc5X4oAxHduuzp2MHYyq2Dq6mrlzlYOhyrffALtM477Wq5jZ-Be67lrXMWYL-i7ugW8OlVcZOlfm3Cd2YfQcs9oLWmkb1M7L6pbFJCeTVVdps3Eg&lib=MeRBDvhNyN2dNnjjFkP1mqD_DLMbnuuk7", {
     method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
     body: JSON.stringify(data)
   })
-    .then(response => response.json())
+    .then(res => res.json())
     .then(result => {
-      alert("Cảm ơn bạn đã gửi đánh giá!");
-      form.reset();
+      alert("Gửi thành công");
+      console.log(result);
     })
-    .catch(error => {
-      alert("Có lỗi xảy ra, vui lòng thử lại!");
-      console.error(error);
+    .catch(err => {
+      alert("Lỗi khi gửi");
+      console.error(err);
     });
 });
-
